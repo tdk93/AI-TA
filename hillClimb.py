8d9b56f929c3a1da78d0cf07cb6c564099693efa
Akshay <akshay.tdk93@gmail.com>
2018-01-25 23:19:12 +0530

    Made the basic skeleton

---
+++ hillClimb.py
@@ -0,0 +1,71 @@
+import math
+
+class Node():
+    def __init__(index, xc, yc):
+        self.i = index
+        self.x = xc
+        self.y = yc
+
+
+def takeInput(fileName):
+    return
+
+def hillClimbOneStep(tour):
+    possible_neighbours = get2OptNeighbours(tour)
+    min_tour_length = getTourLength(tour)
+    localMinima = True 
+    for x in possible_neighbours:
+        if getTourLength(x) < min_tour_length:
+            localMinima = False 
+            min_tour = x
+            min_tour_length = getTourLength(x)
+
+    return min_tour, min_tour_length, localMinima
+
+def hillClimbFull(initial_tour):
+    change = True
+    min_tour_length = 00000000
+    min_tour = 0
+    tour = initial_tour
+    while True:
+        tour, tour_length,localMinima = hillClimbOneStep(tour)
+        if localMinima == True:
+            print("minimum tour found")
+            break
+        else:
+            min_tour_length = tour_length
+            min_tour = tour
+
+    return
+
+def get2OptNeighbours(order):
+    final_neighbours = []
+    for x in range(order.length()):
+        for y in range(order.length()):
+            if x != y:
+                new_order = order[:x] + order[x:y][::-1] + order[y:]
+                final_neighbours.append(new_order)
+    return final_neighbours
+
+def get3OptNeigbours(s):
+    return
+
+def getTourLength(tour):
+    length = 0
+    if tour.length == 2:
+        return getDistance(tour[0],tour[1])
+
+    for x in range(tour.length()-1):
+        length += getDistance(tour[x],tour[x+1]) 
+    
+    length += getDistance(tour[0],tour[-1])
+
+    return length
+
+def getDistance(n1, n2):
+    return math.sqrt((n1.x-n2.x)*(n1.x-n2.x) + (n1.y-n2.y)*(n1.y-n2.y))
+
+def getRandomTour(s):
+    return
+
+
